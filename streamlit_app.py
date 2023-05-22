081a1c0ea71aca2da747dea3f7a3e46f39d94e14
unknown <louiecervantes@wvsu.edu.ph>
2023-05-22 14:56:17 +0800

    save changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,8 @@ def app():
     # Display the text when the user submits the form
     if st.button('Submit'):
         history = append_history(history, ('user: What are research areas in the field of ' + course))
-        output = get_reply('What are research areas in the field of ' + course)
+        prompt = 'What are research areas in the field of ' + course
+        output = get_reply(prompt)
         history = append_history(history, ('Weebsu: ' + output))
         for item in range(len(history)):
             st.write(history[item])
@@ -51,7 +52,8 @@ def app():
         study_area = st.text_area('Copy one of the recommended research areas or input your own:', height=5)
         if st.button('Find Research Topics'):
             history = append_history(history, ('user: What are research topics in the field of ' + study_area))
-            output = get_reply('What are research topics in the field of ' + study_area)
+            prompt = 'What are research topics in the field of ' + study_area
+            output = get_reply(prompt)
             history = append_history(history, ('Weebsu: ' + output))
             for item in range(len(history)):
                 st.write(history[item])
